16fcde85ecdc15964b786844db6a947212204921
Yuki <yuki.utk8g@gmail.com>
2023-08-03 03:52:27 +0300

    add i3 config (with small fonts)

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ ln -sf $PWD/.bashrc ~/.bashrc
 ln -sf $PWD/.gitconfig ~/.gitconfig
 ln -sf $PWD/bash-status-line/shell.sh ~/shell.sh
 ln -sf $PWD/.ssh/authorized_keys ~/.ssh/authorized_keys
+ln -sf $PWD/i3srs.toml ~/i3srs.toml
+ln -sf $PWD/.config/i3/config ~/.config/i3/config
 
 # add own mirror with precompiled AUR packages!
-sudo pacman -S vim-lightline-git vim-youcompleteme-git thefuck
+sudo pacman -S vim-lightline-git vim-youcompleteme-git st i3-wm i3lock dmenu i3status-rust terminus-font ttf-cascadia-code-nerd
 
