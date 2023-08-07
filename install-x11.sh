9caf866b270e58c40a89afea28e1f7c65844fa63
Yuki <yuki.utk8g@gmail.com>
2023-08-07 17:32:15 +0300

    use feh for wallpaper

--- install-x11.sh
+++ install-x11.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+curl https://yukii.keenetic.pro/wall/im.png -o ~/wallpaper.png
+
 ln -sf $PWD/home/i3srs.toml ~/i3srs.toml
 ln -sf $PWD/home/.config/i3/config ~/.config/i3/config
 
 sudo pacman -S i3-wm i3lock i3status-rust \
-  dmenu xcompmgr maim nitrogen st \
+  dmenu feh maim st xcompmgr \
   terminus-font ttf-cascadia-code-nerd \
   xclip xdotool xorg-setxkbmap xorg-xinput
