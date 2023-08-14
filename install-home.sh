1fa28ff6ac133bdec9554a61f17fa2c85d264b09
Yuki <yuki.utk8g@gmail.com>
2023-08-14 20:44:01 +0300

    add verify ssh sig for git

--- install-home.sh
+++ install-home.sh
@@ -3,6 +3,8 @@
 ln -sf $PWD/home/.bashrc ~/.bashrc
 ln -sf $PWD/home/.gitconfig ~/.gitconfig
 ln -sf $PWD/home/.ssh/authorized_keys ~/.ssh/authorized_keys
+mkdir -p ~/.config/git
+ln -sf $PWD/home/.config/git/allowed_signers ~/.config/git/allowed_signers
 
 sudo pacman -S bat exa git-delta ripgrep
 
