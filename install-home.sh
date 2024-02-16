a1a7ffbf5c61ca893d1b1cba0707319837891d8e
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-02-16 22:18:34 +0300

    simplify eww config paths

--- install-home.sh
+++ install-home.sh
@@ -3,13 +3,12 @@
 yay -Syyu swaylock kitty grim lxsession-gtk3 dunst \
   terminus-font ttf-cascadia-code-nerd ttf-fantasque-sans-mono noto-fonts noto-fonts-cjk \
   qt5ct krita firefox qalculate-gtk transmission-qt mpv cmus ffmpeg rustup \
-  grimshot sway-im eww-wayland rofi-lbonn-wayland sublime-text-4
+  grimshot sway-im eww-tray-wayland-git rofi-lbonn-wayland sublime-text-4
 
-mkdir -p ~/.ssh ~/.config ~/.config/eww ~/.config/sublime-text
+mkdir -p ~/.ssh ~/.config ~/.config/sublime-text
 
 ln -sf $PWD/home/.{bashrc,gitconfig} ~/
 ln -sf $PWD/home/.ssh/authorized_keys ~/.ssh/
-ln -sf $PWD/home/.config/eww/bar ~/.config/eww/
 ln -sf $PWD/home/.config/sublime-text/Packages ~/.config/sublime-text/
 
-ln -sf $PWD/home/.config/{dunst,git,kitty,rofi,qt5ct,sway} ~/.config/
+ln -sf $PWD/home/.config/{dunst,eww,git,kitty,rofi,qt5ct,sway} ~/.config/
