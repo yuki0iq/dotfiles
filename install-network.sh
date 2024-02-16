847c8f3f9c60bd01488a151fcf4da48f036f595c
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-02-16 20:35:35 +0300

    force ssh-ed25519 and make ssh-audit pass as green

--- install-network.sh
+++ install-network.sh
@@ -4,7 +4,6 @@ pacman -S iwd openssh
 
 # created as `diff -Naur sysd yuki > network.patch`
 patch -d / -Np1 < patch/network.patch
-patch -d / -Np1 < patch/ssh.patch
 
 install -m0644 \
   root/etc/systemd/network/20-wired.network \
@@ -12,4 +11,9 @@ install -m0644 \
   root/etc/systemd/network/25-wireless.network \
   /etc/systemd/network
 
-systemctl enable --now systemd-timesyncd systemd-networkd systemd-resolved iwd
+install -m0644 \
+  root/etc/ssh/ssh_config \
+  root/etc/ssh/sshd_config \
+  /etc/ssh
+
+systemctl enable --now systemd-timesyncd systemd-networkd systemd-resolved iwd sshd
