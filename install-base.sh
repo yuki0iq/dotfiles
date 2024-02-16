c1e764a5d644913632d8c3a60ddb7d81425da59c
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-02-16 21:58:40 +0300

    fuck me sideways i accidentally deleted my ~/.config and lost sway-polybar...

--- install-base.sh
+++ install-base.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pacman -S bash patch sudo which bat eza git-delta ripgrep bind curl inetutils iproute2 iputils \
+pacman -Syyu bash patch sudo which bat eza git-delta ripgrep bind curl inetutils iproute2 iputils \
   inxi neofetch onefetch tokei htop ncdu tmux vim mc lsof strace git openssh speedtest-cli w3m \
   iwd ly-dm kmsconvt
 
@@ -9,7 +9,7 @@ pacman -S bash patch sudo which bat eza git-delta ripgrep bind curl inetutils ip
 # created as `diff -Naur sysd yuki > network.patch`
 patch -d / -Np1 < patch/network.patch
 
-mkdir /etc/kmscon
+mkdir -p /etc/kmscon
 
 install -m0644 root/etc/kmscon/kmscon.conf /etc/kmscon
 install -m0644 root/etc/systemd/network/{20-wired,21-loopback,25-wireless}.network /etc/systemd/network
--- install-home.sh
+++ install-home.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-yay -S sway swaylock eww-wayland rofi-lbonn-wayland kitty grim lxsession-gtk3 dunst \
-  terminus-font ttf-cascadia-code-nerd ttf-fantasque-sans-mono noto-fonts noto-fonts-cjk \
-  qt5ct sublime-text-4 krita firefox qalculate-gtk transmission-qt mpv cmus ffmpeg rustup
 
-mkdir ~/.ssh ~/.config ~/.config/eww ~/.config/sublime-text
+# sway-im eww-wayland rofi-lbonn-wayland sublime-text-4
+yay -Syyu swaylock kitty grim lxsession-gtk3 dunst \
+  terminus-font ttf-cascadia-code-nerd ttf-fantasque-sans-mono noto-fonts noto-fonts-cjk \
+  qt5ct krita firefox qalculate-gtk transmission-qt mpv cmus ffmpeg rustup
+
+mkdir -p ~/.ssh ~/.config ~/.config/eww ~/.config/sublime-text
 
 ln -sf $PWD/home/.{bashrc,gitconfig} ~/
 ln -sf $PWD/home/.ssh/authorized_keys ~/.ssh/
