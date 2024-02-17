10b2df1d6ce0ef9ac2f5101c656bba8107b85f99
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-02-17 19:32:50 +0300

    do not use kmsconvt it breaks something

--- install-base.sh
+++ install-base.sh
@@ -2,18 +2,15 @@
 
 pacman -Syyu bash patch sudo which bat eza git-delta ripgrep bind curl inetutils iproute2 iputils \
   inxi neofetch onefetch tokei htop ncdu tmux vim mc lsof strace git openssh speedtest-cli w3m \
-  iwd ly-dm kmsconvt
+  iwd ly-dm
 
 # TODO install yay
 
 # created as `diff -Naur sysd yuki > network.patch`
 patch -d / -Np1 < patch/network.patch
 
-mkdir -p /etc/kmscon
 
-install -m0644 root/etc/kmscon/kmscon.conf /etc/kmscon
 install -m0644 root/etc/systemd/network/{20-wired,21-loopback,25-wireless}.network /etc/systemd/network
 install -m0644 root/etc/ssh/{ssh,sshd}_config /etc/ssh
-install -m0644 root/etc/environment /etc
 
 systemctl enable --now systemd-timesyncd systemd-networkd systemd-resolved iwd sshd ly-dm
