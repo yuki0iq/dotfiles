fa46aec8eb4648f3bc2507525ccdd2c00f5fc74e
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-02-17 19:25:36 +0300

    use gtk3 theming... qt5ct still cringe

--- install-home.sh
+++ install-home.sh
@@ -2,7 +2,7 @@
 
 yay -Syyu swaylock kitty grim lxsession-gtk3 dunst \
   terminus-font ttf-cascadia-code-nerd ttf-fantasque-sans-mono noto-fonts noto-fonts-cjk \
-  qt5ct krita firefox qalculate-gtk transmission-qt mpv cmus ffmpeg rustup \
+  qt5ct nwg-look krita firefox qalculate-gtk transmission-qt mpv cmus ffmpeg rustup \
   grimshot sway-im eww-tray-wayland-git rofi-lbonn-wayland sublime-text-4
 
 mkdir -p ~/.ssh ~/.config ~/.config/sublime-text
@@ -11,4 +11,4 @@ ln -sf $PWD/home/.{bashrc,gitconfig} ~/
 ln -sf $PWD/home/.ssh/authorized_keys ~/.ssh/
 ln -sf $PWD/home/.config/sublime-text/Packages ~/.config/sublime-text/
 
-ln -sf $PWD/home/.config/{dunst,eww,git,kitty,rofi,qt5ct,sway} ~/.config/
+ln -sf $PWD/home/.config/{dunst,environment.d,eww,git,gtk-3.0,kitty,nwg-look,rofi,qt5ct,sway} ~/.config/
