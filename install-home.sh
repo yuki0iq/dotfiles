026b4a8a2eaf71c3cee0931747851eefd12a47df
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-02-19 18:28:41 +0300

    add dconf and fix some settings

--- install-home.sh
+++ install-home.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 yay -Syyu sway-im swaylock kitty lxsession-gtk3 dunst grimshot eww-tray-wayland-git rofi-lbonn-wayland \
-  gnome-desktop breeze inotify-tools nwg-look qt5ct \
+  gnome-desktop breeze inotify-tools nwg-look qt5ct dconf-editor \
   terminus-font ttf-cascadia-code-nerd ttf-fantasque-sans-mono noto-fonts noto-fonts-cjk \
   krita firefox qalculate-gtk transmission-qt mpv cmus ffmpeg rustup sublime-text-4
 
