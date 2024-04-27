3e02cb991e4c86393f4b38e57f5c9e5de503f7ae
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-04-27 22:51:40 +0300

    rofi-wayland is now in arch packages

--- install-home.sh
+++ install-home.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 paru -Syu \
-  sway-im swaylock-effects eww-tray-wayland-git rofi-lbonn-wayland \
+  sway-im swaylock-effects eww-tray-wayland-git rofi-wayland \
   mate-polkit dunst grimshot inotify-tools \
   fcitx5-{configtool,gtk,mozc} \
   gnome-desktop breeze nwg-look materia-gtk-theme qt6ct \
   terminus-font{,-italic} ttf-nerd-fonts-symbols ttf-fantasque-sans-mono noto-fonts{,-cjk} \
   wayvnc waypipe firefox kitty gef sublime-text-4 keepassxc \
+  xdg-desktop-portal-gtk \
   krita qbittorrent cmus ffmpeg mpv
 
 mkdir -p ~/.ssh ~/.config ~/.config/sublime-text ~/.local ~/.local/share ~/.local/state
