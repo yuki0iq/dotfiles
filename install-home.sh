e2847ce48b6f9aa6822fa9338541c85834fbb26f
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-05-14 01:08:47 +0300

    Replace gnome-terminal-transparency with foot
    Turns out foot has even smaller footprint (~1.5 MiB on instance and
    approximately 15 MiB on server). It does not support ligatures, *but*
    audible and visual bell work (with some hacks).
    
    I could not find what is the cause for libvte3-based terminals not
    playing bell sound from xdg sound theme, when xterm and kitty could
    play the sound.

--- install-home.sh
+++ install-home.sh
@@ -5,9 +5,9 @@ paru -Syu \
   mate-polkit dunst grimshot inotify-tools \
   gnome-desktop breeze nwg-look materia-gtk-theme qt6ct-kde grc \
   terminus-font{,-italic} ttf-nerd-fonts-symbols ttf-fantasque-sans-mono noto-fonts{,-cjk} \
-  wayvnc waypipe firefox gnome-terminal-transparency gef sublime-text-4 keepassxc \
+  wayvnc waypipe firefox foot gef sublime-text-4 keepassxc \
   xdg-desktop-portal-gtk \
-  krita qbittorrent cmus ffmpeg mpv \
+  krita qbittorrent cmus ffmpeg mpv sound-theme-freedesktop \
   power-profiles-daemon python-gobject
 
 mkdir -p ~/.ssh ~/.config ~/.config/sublime-text ~/.local ~/.local/share ~/.local/state
@@ -16,4 +16,4 @@ ln -sf $PWD/home/.{bash{_profile,rc},xkb} ~/
 ln -sf $PWD/home/.ssh/authorized_keys ~/.ssh/
 ln -sf $PWD/home/.config/sublime-text/Packages ~/.config/sublime-text/
 
-ln -sf $PWD/home/.config/{dconf,dunst,easyeffects,eww,fcitx5,gdb,git,gtk-3.0,kitty,MangoHud,nvim,nwg-look,paru,qt6ct,rofi,sway} ~/.config/
+ln -sf $PWD/home/.config/{dconf,dunst,easyeffects,eww,foot,gdb,git,gtk-3.0,MangoHud,nvim,nwg-look,paru,qt6ct,rofi,sway} ~/.config/
