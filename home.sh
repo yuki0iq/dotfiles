86cf21454cd539886573b777b775efaaef52a473
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-05-24 14:17:34 +0300

    home: Mark some packages as dependencies

--- home.sh
+++ home.sh
@@ -11,9 +11,12 @@ paru -Syu \
   foot grc gef sublime-text-4 \
   xdg-desktop-portal{,-wlr,-gtk} keepassxc \
   pipewire{,-pulse,-alsa,-jack} wireplumber cmus ffmpeg mpv sound-theme-freedesktop \
-  krita qbittorrent thunar gvfs{,-gphoto2,-mtp} imv engrampa \
-  power-profiles-daemon python-gobject \
+  krita qbittorrent thunar imv engrampa \
+  power-profiles-daemon \
   fcitx5-im fcitx5-mozc
+
+paru -S --asdeps \
+  thunar-{archive-plugin,volman}tumbler gvfs{,-gphoto2,-mtp} python-gobject
 
 mkdir -p ~/.ssh ~/.config ~/.config/sublime-text ~/.local ~/.local/share ~/.local/state
 
