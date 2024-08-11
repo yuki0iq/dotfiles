6a3b94b5bc0559cd28336583758c7b94e57133ea
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-08-11 14:49:17 +0300

    nvim: Use native lsp config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -29,7 +29,6 @@ require("lazy").setup({
     },
     {
         "nvim-tree/nvim-tree.lua",
-        version = "*",
         lazy = false,
         dependencies = {
             "nvim-tree/nvim-web-devicons",
@@ -48,42 +47,192 @@ require("lazy").setup({
     {
         'ojroques/nvim-hardline',
         config = function()
-          require("hardline").setup {}
+            require("hardline").setup {}
         end,
     },
     {
-        'xiyaowong/virtcolumn.nvim'
+        "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+        lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+        dependencies = {
+            -- main one
+            { "ms-jpq/coq_nvim", branch = "coq" },
+
+            -- 9000+ Snippets
+            { "ms-jpq/coq.artifacts", branch = "artifacts" },
+
+            -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+            -- Need to **configure separately**
+            { 'ms-jpq/coq.thirdparty', branch = "3p" },
+            -- - shell repl
+            -- - nvim lua api
+            -- - scientific calculator
+            -- - comment banner
+            -- - etc
+        },
+        init = function()
+            vim.g.coq_settings = {
+                auto_start = 'shut-up', -- if you want to start COQ at startup
+                -- Your COQ settings here
+            }
+        end,
+        config = function()
+            -- Your LSP settings here
+            local lsp = require("lspconfig")
+
+            vim.api.nvim_create_autocmd('LspNotify', {
+                callback = function(args)
+                    local bufnr = args.buf
+                    local client_id = args.data.client_id
+                    local method = args.data.method
+                    local params = args.data.params
+                    -- do something with the notification
+                    if method == 'textDocument/...' then
+                        update_buffer(bufnr)
+                    end
+                end,
+            })
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    local opts = { buffer = args.buf, noremap = true, silent = true }
+
+                    if client.supports_method('textDocument/completion') then
+                        -- Enable auto-completion
+                        -- vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+                    end
+
+                    if client.supports_method('textDocument/formatting') then
+                        -- Format the current buffer on save
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            buffer = args.buf,
+                            callback = function()
+                                vim.lsp.buf.format({bufnr = args.buf, id = client.id})
+                            end,
+                        })
+                    end
+
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+
+                    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set('n', '<space>wl', function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
+
+                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
+                    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+                    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+                    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+                    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+                    vim.lsp.inlay_hint.enable(true, {
+                        bufnr = args.buf,
+                    })
+                end,
+            })
+
+            vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
+                group = vim.api.nvim_create_augroup("code_action_sign", { clear = true }),
+                callback = function()
+                    local context = { diagnostics = vim.lsp.diagnostic.get_line_diagnostics() }
+                    local params = vim.lsp.util.make_range_params()
+                    params.context = context
+                    vim.lsp.buf_request(0, 'textDocument/codeAction', params,
+                        function(err, result, ctx, config)
+                            -- do something with result
+                            -- e.g. check if empty and show some indication such as a sign
+                        end
+                    )
+                end,
+            })
+
+            lsp.clangd.setup {}
+        end,
     },
---    {
---        'Valloric/YouCompleteMe',
---        build = 'python3 install.py --verbose --clang-completer --rust-completer'
---    },  -- why not building?
     {
-        'kaarmu/typst.vim',
-        ft = 'typst',
-        lazy = false,
-    }
+        'mrcjkb/rustaceanvim',
+        lazy = false, -- This plugin is already lazy
+
+        init = function()
+            vim.g.rustaceanvim = {
+                -- Plugin configuration
+                tools = {
+                },
+
+                -- LSP configuration
+                server = {
+                    cmd = {
+                        "/usr/bin/env",
+                        "CARGO_HOME=/home/yuki/.local/share/cargo",
+                        "RUSTUP_HOME=/home/yuki/.local/share/rustup",
+                        "/usr/bin/rustup",
+                        "run",
+                        "nightly",
+                        "rust-analyzer",
+                    },
+
+                    on_attach = function(client, bufnr)
+                        local opts = { buffer = bufnr, noremap = true, silent = true }
+
+                        vim.keymap.set('n', '<space>a', function()
+                            vim.cmd.RustLsp('codeAction')
+                        end, opts)
+
+                        vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
+                            group = vim.api.nvim_create_augroup("code_action_sign", {
+                                clear = true
+                            }),
+                            callback = function()
+                                vim.cmd.RustLsp { 'hover', 'actions' }
+                            end,
+                        })
+                    end,
+
+                    default_settings = {
+                        ['rust-analyzer'] = {
+                            inlayHints = {
+                                lifetimeElisionHints = {
+                                    enable = true,
+                                    useParameterNames = true,
+                                },
+                            },
+                            checkOnSave = true,
+                            check = {
+                                enable = true,
+                                command = 'clippy',
+                                features = 'all',
+                            },
+                            procMacro = {
+                                enable = true,
+                            },
+                        },
+                    },
+                },
+
+                -- DAP configuration
+                dap = {
+                },
+            }
+        end,
+        -- vim.cmd.RustLsp({ 'renderDiagnostic', 'cycle' })
+    },
+    {
+        'xiyaowong/virtcolumn.nvim',
+        init = function()
+            vim.g.virtcolumn_char = '|'
+            vim.g.virtcolumn_priority = 10
+        end,
+    },
 }, {
-    git = { 
+    git = {
         timeout = 3600, -- kill after AN HOUR instead of 2 minutes
     }
 })
-
-vim.g.virtcolumn_char = '▕' -- char to display the line
-vim.g.virtcolumn_priority = 10 -- priority of extmark
-
---vim.cmd [[nmap <leader>yfw <Plug>(YCMFindSymbolInWorkspace)]]
---vim.cmd [[nmap <leader>yfd <Plug>(YCMFindSymbolInDocument)]]
---vim.cmd [[nmap <F3> :YcmCompleter Format<CR>]]
---vim.cmd [[nmap <F1> :tab YcmCompleter GetDoc<CR>]]
---vim.cmd [[nmap <C-t> :YcmCompleter GetType<CR>]]
---vim.cmd [[nmap <leader>gg :YcmCompleter GoTo<CR>]]
---vim.cmd [[nmap <leader>gu :YcmCompleter GoToReferences<CR>]]
-
---vim.cmd [[let g:ycm_language_server = [{
---    \   'name': 'ccls',
---    \   'cmdline': [ 'ccls' ],
---    \   'filetypes': [ 'c', 'cpp', 'cuda', 'objc', 'objcpp' ],
---    \   'project_root_files': [ '.ccls-root', 'compile-commands.json', 'build/compile-commands.json' ],
---    \}]
---]]
