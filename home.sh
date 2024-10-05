ffb257148510adda0684caef5a493421c03f14c7
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-10-05 17:34:26 +0300

    home: Okular, fooyin, something more?

--- home.sh
+++ home.sh
@@ -1,26 +1,19 @@
 #!/bin/sh
 
+paru -S --asdeps tumbler python-gobject phonon-qt6-mpv
+
 paru -Syu \
-  sway-git sway-systemd swaylock-effects rofi-wayland tcc \
-  mate-polkit dunst grimshot \
-  nwg-look materia-gtk-theme \
-  qt5ct-kde breeze5 \
-  qt6ct-kde breeze \
-  papirus-icon-theme \
-  terminus-font{,-italic} ttf-nerd-fonts-symbols ttf-fantasque-sans-mono noto-fonts{,-cjk} \
-  wayvnc waypipe wdisplays \
-  firefox \
-  xdg-terminal-exec-mkhl foot grc gef sublime-text-4 \
-  xdg-desktop-portal{,-wlr,-gtk} \
-  pipewire{,-pulse,-alsa,-jack} wireplumber \
-  cmus ffmpeg mpv \
-  sound-theme-freedesktop \
-  krita qbittorrent thunar imv engrampa \
+  sway-git sway-systemd swaylock-effects xdg-desktop-portal{,-wlr,-gtk} foot \
+  pipewire{,-pulse,-alsa,-jack} wireplumber sound-theme-freedesktop ffmpeg mpv fooyin-git \
+  rofi-wayland xdg-terminal-exec-mkhl mate-polkit dunst grimshot \
+  nwg-look materia-gtk-theme papirus-icon-theme qt5ct-kde breeze5 qt6ct-kde breeze grc \
+  ttf-nerd-fonts-symbols ttf-fantasque-sans-mono ttf-dejavu noto-fonts{,-cjk} \
+  wayvnc waypipe-git wdisplays \
+  firefox krita qbittorrent imv okular sublime-text-4 \
+  thunar thunar-{archive-plugin,volman} gvfs{,-gphoto2,-mtp} engrampa \
+  gef \
   power-profiles-daemon \
   fcitx5-im fcitx5-mozc
-
-paru -S --asdeps \
-  thunar-{archive-plugin,volman}tumbler gvfs{,-gphoto2,-mtp} python-gobject
 
 mkdir -p ~/.ssh ~/.local ~/.local/share ~/.local/state
 
