286e398780f480060dca22bf28fab24317460a11
Yuki Sireneva <yuki.utk8g@gmail.com>
2024-10-11 15:41:42 +0300

    home: dont use mate-polkit

--- home.sh
+++ home.sh
@@ -5,7 +5,7 @@ paru -S --asdeps tumbler python-gobject phonon-qt6-mpv
 paru -Syu \
   sway-git sway-systemd swaylock-effects xdg-desktop-portal{,-wlr,-gtk} foot \
   pipewire{,-pulse,-alsa,-jack} wireplumber sound-theme-freedesktop ffmpeg mpv fooyin-git \
-  rofi-wayland xdg-terminal-exec-mkhl mate-polkit dunst grimshot \
+  rofi-wayland xdg-terminal-exec-mkhl dunst grimshot \
   nwg-look materia-gtk-theme papirus-icon-theme qt5ct-kde breeze5 qt6ct-kde breeze grc \
   ttf-nerd-fonts-symbols ttf-fantasque-sans-mono ttf-dejavu noto-fonts{,-cjk} \
   wayvnc waypipe-git wdisplays \
