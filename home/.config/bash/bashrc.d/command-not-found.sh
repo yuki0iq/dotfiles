8b2bde820129b565a992b94fd854256cf3197681
Yuki Sireneva <yuki.utk8g@gmail.com>
2025-02-21 13:03:38 +0300

    bash/c-n-f: Fixes

--- home/.config/bash/bashrc.d/command-not-found.sh
+++ home/.config/bash/bashrc.d/command-not-found.sh
@@ -5,7 +5,7 @@ command_not_found_handle () {
 
     if busybox --list | grep ^$cmd\$ >/dev/null 2>&1; then
         if tty -s; then
-            printf "\e[33m\e[3m--> Using busybox for `%s`\e[m\n" $cmd
+            printf "\e[33m\e[3m--> Using busybox for '%s'\e[m\n" $cmd
         fi
         exec busybox "$@"
     else
