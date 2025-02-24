d56ec1ae6f29887cb0bc017575c215aa4caf7c04
Yuki Sireneva <yuki.utk8g@gmail.com>
2025-02-24 19:03:58 +0300

    dconf: Fix ibus layouts, use jp+ipa for Super+F10

--- home.sh
+++ home.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 paru -Syu \
-  gdm gnome-shell gnome-control-center refine-git ibus-anthy \
+  gdm gnome-shell gnome-control-center refine-git ibus-anthy ibus-table-extra \
   gnome-shell-extensions gnome-shell-extension-{caffeine,legacy-theme-auto-switcher-git} \
   gst-plugin-pipewire gst-plugins-bad gnome-remote-desktop fwupd malcontent \
   pipewire{,-pulse,-alsa,-jack} wireplumber sound-theme-freedesktop ffmpeg mpv fooyin-git \
-  foot xdg-terminal-exec-mkhl grc gef \
+  foot xdg-terminal-exec-mkhl grc gef wl-clipboard \
   qt5ct-kde breeze5 qt6ct-kde breeze adw-gtk-theme \
-  ttf-nerd-fonts-symbols ttf-fantasque-sans-mono ttf-dejavu noto-fonts{,-cjk} \
-  wayvnc waypipe-git wl-clipboard \
+  ttf-nerd-fonts-symbols ttf-fantasque-sans-mono noto-fonts{,-cjk} \
   firefox krita kolourpaint qbittorrent sublime-text-4 obs-studio \
   nautilus gvfs{,-gphoto2,-mtp} file-roller loupe identity papers foliate \
   power-profiles-daemon
