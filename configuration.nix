8246ff686e43827fa852a4b3c175e3aed9f001cd
Yuki Sireneva <yuki.utk8g@gmail.com>
2025-04-22 05:57:47 +0300

    Fix modeline

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# vim: set et ts=2 sw=2
+# vim:et:ts=2:sw=2
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
