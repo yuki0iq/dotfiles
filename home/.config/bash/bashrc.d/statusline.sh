17ed9c85cbbde9cc8b4a56f238890adcfbb6d6e8
Yuki Sireneva <yuki.utk8g@gmail.com>
2025-05-13 00:03:48 +0300

    bashrc: Update statusline

--- home/.config/bash/bashrc.d/statusline.sh
+++ home/.config/bash/bashrc.d/statusline.sh
@@ -1,15 +1,4 @@
-case $TERM in
-    linux|tmux-*)
-        PS1_MODE=text
-        ;;
-    *)
-        PS1_MODE=minimal
-        ;;
-esac
-export PS1_MODE
-
 if command -v statusline >/dev/null; then
-    # statusline does the job for bash
-    export MAILCHECK=-1
-    eval "$(statusline env)"
+    export PS1_MODE=minimal
+    source <(statusline env)
 fi
