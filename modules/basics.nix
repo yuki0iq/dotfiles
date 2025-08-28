8300172f02327bb68cbcc214e19634038da392a5
Yuki Sireneva <yuki.utk8g@gmail.com>
2025-08-28 21:05:41 +0300

    Make shell aliases available to all shells

--- modules/basics.nix
+++ modules/basics.nix
@@ -14,6 +14,17 @@ in {
   };
 
   config = lib.mkIf config.meow.basics {
+    environment.shellAliases = {
+      downspeed = "${pkgs.iperf3}/bin/iperf3 -c iperf3.moji.fr -p 5225";
+      upspeed = "${pkgs.iperf3}/bin/iperf3 -c iperf3.moji.fr -p 5225 -R";
+      cat = "${pkgs.bat}/bin/bat";
+      ip = "ip -c=always";
+      ls = "${pkgs.eza}/bin/eza --color=auto --hyperlink";
+      diff = "diff --color=auto";
+      psu = "ps ouser:8,tid:6,pri,bsdtime:6,pss:10,rss:10,uss:10,oom,tt:5,stat,ucmd";
+      psc = "ps ouser:8,tid:6,pri,bsdtime:6,pss:10,rss:10,uss:10,oom,tt:5,stat,cmd";
+    };
+
     environment.systemPackages = with pkgs; [
       man-pages
       man-pages-posix
@@ -44,17 +55,6 @@ in {
         PS1_MODE=minimal
         eval "$(${pkgs.statusline}/bin/statusline env)"
       '';
-
-      shellAliases = {
-        downspeed = "${pkgs.iperf3}/bin/iperf3 -c iperf3.moji.fr -p 5225";
-        upspeed = "${pkgs.iperf3}/bin/iperf3 -c iperf3.moji.fr -p 5225 -R";
-        cat = "${pkgs.bat}/bin/bat";
-        ip = "ip -c=always";
-        ls = "${pkgs.eza}/bin/eza --color=auto --hyperlink";
-        diff = "diff --color=auto";
-        psu = "ps ouser:8,tid:6,pri,bsdtime:6,pss:10,rss:10,uss:10,oom,tt:5,stat,ucmd";
-        psc = "ps ouser:8,tid:6,pri,bsdtime:6,pss:10,rss:10,uss:10,oom,tt:5,stat,cmd";
-      };
 
       vteIntegration = false;
     };
