8dbb335057f428b7438d7a244bc01ca362062619
Yuki Sireneva <yuki.utk8g@gmail.com>
2025-11-20 11:21:55 +0300

    Autoimport of nixosConfigurations and nixos modules

--- default.nix
+++ default.nix
@@ -4,20 +4,12 @@ let
   pins = builtins.mapAttrs (name: pinned: pinned {inherit pkgs;}) bare_pins;
 
   lib = pkgs.lib;
-  evalConfig = import "${pins.nixpkgs}/nixos/lib/eval-config.nix";
-  nixosSystem = host:
-    evalConfig {
-      specialArgs = {
-        inherit pins;
-      };
-
-      modules = [
-        ./modules
-        ./hosts/${host}
-      ];
+  readDir' = dir: lib.mapAttrsToList (name: _: lib.path.append dir name) (builtins.readDir dir);
+  nixosSystem = name:
+    (import "${pins.nixpkgs}/nixos/lib/eval-config.nix") {
+      specialArgs = {inherit pins;};
+      modules = (readDir' ./modules) ++ [./hosts/${name}];
     };
 in {
-  nixosConfigurations = lib.flip lib.genAttrs nixosSystem [
-    "yuuka"
-  ];
+  nixosConfigurations = lib.mapAttrs (name: _: nixosSystem name) (builtins.readDir ./hosts);
 }
