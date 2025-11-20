c5aceb58f7cf1edf09177bd9463ab921d0056a98
Yuki Sireneva <yuki.utk8g@gmail.com>
2025-11-20 10:13:38 +0300

    Switch LC_TIME to more widely supported en_GB

--- modules/system.nix
+++ modules/system.nix
@@ -12,7 +12,7 @@
     i18n.extraLocaleSettings = {
       LC_MEASUREMENT = "C.UTF-8";
       LC_PAPER = "C.UTF-8";
-      LC_TIME = "en_DK.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
     };
     i18n.extraLocales = "all";
 
