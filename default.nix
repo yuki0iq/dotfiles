3a7b4be830d413ceffc37840db5241c5cb655169
Yuki Sireneva <yuki.utk8g@gmail.com>
2025-12-02 14:03:30 +0300

    Use `<nixpkgs/nixos>` instead of `<nixpkgs/nixos/lib/eval-config.nix>`

--- default.nix
+++ default.nix
@@ -6,9 +6,9 @@ let
   lib = pkgs.lib;
   readDir' = dir: lib.mapAttrsToList (name: _: lib.path.append dir name) (builtins.readDir dir);
   nixosSystem = name:
-    (import "${pins.nixpkgs}/nixos/lib/eval-config.nix") {
+    (import "${pins.nixpkgs}/nixos") {
       specialArgs = {inherit pins;};
-      modules = (readDir' ./modules) ++ [./hosts/${name}];
+      configuration.imports = (readDir' ./modules) ++ [./hosts/${name}];
     };
 in {
   nixosConfigurations = lib.mapAttrs (name: _: nixosSystem name) (builtins.readDir ./hosts);
