ac9cccbf535bca92cb99deded959ea2162d73272
Yuki Sireneva <yuki.utk8g@gmail.com>
2026-01-07 04:25:51 +0300

    Replace libressl netcat with netcat-openbsd

--- modules/network.nix
+++ modules/network.nix
@@ -6,10 +6,13 @@
 }: {
   options.meow.network.enable = lib.mkEnableOption "network tools and services" // {default = true;};
 
+  options.environment.corePackages = lib.mkOption { apply = lib.subtractLists [pkgs.netcat]; };
+
   config = lib.mkIf config.meow.network.enable {
     environment.systemPackages = with pkgs; [
       curl
       iperf3
+      netcat-openbsd
       nmap
       rsync
       socat
