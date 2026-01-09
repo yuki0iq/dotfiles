d7aa32300718b84e0fc5b4974f636a085f1912b0
Yuki Sireneva <yuki.utk8g@gmail.com>
2026-01-09 03:01:45 +0300

    Use nftables instead of iptables
    Docker 29 supports using nftables

--- modules/system.nix
+++ modules/system.nix
@@ -23,15 +23,7 @@
       };
     };
 
-    # Configure network proxy if necessary
-    # networking.proxy.default = "http://user:password@proxy:port/";
-    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
+    networking.nftables.enable = true;
 
     time.timeZone = "Europe/Moscow";
 
