4132f98da72a6d7ca23ac9cefe0e0c6e694b5de9
Yuki Sireneva <yuki.utk8g@gmail.com>
2026-02-02 22:06:46 +0300

    install limine as a fallback bootloader

--- profiles/boot.nix
+++ profiles/boot.nix
@@ -1,7 +1,10 @@
 self: {...}: {
   imports = [self.profiles.kernel];
 
-  boot.loader.limine.enable = true;
+  boot.loader.limine = {
+    enable = true;
+    efiInstallAsRemovable = true;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
   services.fwupd.enable = true;
 }
